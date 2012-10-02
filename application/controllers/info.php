901dfd22644a18b66328c9cb69f3e04b8b685ba0
srutto <sharon@ushahidi.com>
2012-10-03 00:06:33 +0300

    New uchaguzi helper and sub tabs added for the Info page

--- application/controllers/info.php
+++ application/controllers/info.php
@@ -14,25 +14,67 @@
  */
 
 class Info_Controller extends Main_Controller {
+	
 	function __construct()
 	{
 		parent::__construct();
 	}
 
-	public function index()
+	public function index($page_id=1)
 	{
+	
+		$menu = "";
+		
+		$page = ORM::factory('page',$page_id)->find($page_id);
+		if ($page->loaded)
+		{
+			$page_title = $page->page_title;
+			$page_description = $page->page_description;
+			$page_id = $page->id;
+		
+			
+		}
 
-		$this->template->header->this_page = 'info';
-		$this->template->content = new View('info');
+		if ($page_id=1)
+		
+		{
 
-		$this->template->header->page_title .= Kohana::lang('uchaguzi.info') . Kohana::config('settings.title_delimiter');
+			
+			$this->template->header->this_page = "page_".$page_id;
+			$this->template->content = new View('info');
+			
+			Event::run('ushahidi_filter.page_title', $page_title);
+			Event::run('ushahidi_filter.page_description', $page_description);
+
+			$this->template->content->page_title = $page_title;
+			$this->template->content->page_description = $page_description;
+			$this->template->content->page_id = $page->id;
+		}
 		
-		$electoral = TRUE;
-		$voting = TRUE;
+		else if ($page_id=2)
 		
-		$this->template->content->electoral = $electoral;
-		$this->template->content->voting = $voting;
+		{
+			
+			$this->template->header->this_page = "page_".$page_id;
+			$this->template->content = new View('info');
+			
+			Event::run('ushahidi_filter.page_title', $page_title);
+			Event::run('ushahidi_filter.page_description', $page_description);
+
+			$this->template->content->page_title = $page_title;
+			$this->template->content->page_description = $page_description;
+			$this->template->content->page_id = $page->id;
+		}
 		
+		else
+		{
+			url::redirect('main');
+		}
+
+		$this->template->header->page_title .= $page_title.Kohana::config('settings.title_delimiter');
+
+		$this->template->header->header_block = $this->themes->header_block();
+		$this->template->footer->footer_block = $this->themes->footer_block();
 	}
 
 }
