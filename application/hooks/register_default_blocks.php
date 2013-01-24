cd4f0347aceb35b9f355a3efd7d1a8293e4bc195
Rob Baker <rrbaker@gmail.com>
2013-01-24 13:11:03 +0300

    created add added blocks on homepage for latest news and highlights

--- application/hooks/register_default_blocks.php
+++ application/hooks/register_default_blocks.php
@@ -1,49 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class reports_block {
-
+class latest_news {
 	public function __construct()
 	{
 		$block = array(
-			"classname" => "reports_block",
-			"name" => "Reports",
-			"description" => "List the 10 latest reports in the system"
-		);
-
-		blocks::register($block);
-	}
-
-	public function block()
-	{
-		$content = new View('blocks/main_reports');
-
-		// Get Reports
-        // XXX: Might need to replace magic no. 8 with a constant
-		$content->total_items = ORM::factory('incident')
-			->where('incident_active', '1')
-			->limit('8')->count_all();
-		$content->incidents = ORM::factory('incident')
-			->with('location')
-			->where('incident_active', '1')
-			->limit('10')
-			->orderby('incident_date', 'desc')
-			->find_all();
-
-		echo $content;
-	}
-}
-
-new reports_block;
-
-
-class news_block {
-
-	public function __construct()
-	{
-		$block = array(
-			"classname" => "news_block",
-			"name" => "Main Stream News",
-			"description" => "List the 10 latest news items from available news feeds"
+			"classname" => "latest",
+			"name" => "Latest News",
+			"description" => "Displays the most recent news story from the feeds"
 		);
 
 		blocks::register($block);
@@ -54,12 +17,42 @@ class news_block {
 		$content = new View('blocks/main_news');
 		// Get RSS News Feeds
 		$content->feeds = ORM::factory('feed_item')
-			->limit('10')
+			->limit('1')
 			->orderby('item_date', 'desc')
 			->find_all();
 
 		echo $content;
 	}
 }
+new latest_news;
 
-new news_block;
+
+class highlights {
+	public function __construct()
+	{
+		$block = array(
+			"classname" => "highlights",
+			"name" => "Highlights",
+			"description" => "List the three most recent, flagged reports"
+		);
+
+		blocks::register($block);
+	}
+
+	public function block()
+	{
+		$content = new View('blocks/main_highlights');
+		$content->total_items = ORM::factory('incident')
+			->where('incident_active', '1')
+			->limit('3')->count_all();
+		$content->incidents = ORM::factory('incident')
+			->with('location')
+			->where('incident_active', '1')
+			->limit('3')
+			->orderby('incident_date', 'desc')
+			->find_all();
+
+		echo $content;
+	}
+}
+new highlights;
---
+++ themes/uchaguzi/views/blocks/main_highlights.php
@@ -0,0 +1,32 @@
+<?php blocks::open("highlights");?>
+
+<?php blocks::title(Kohana::lang('uchaguzi.highlights'));?>
+<?php
+if ($incidents->count() == 0)
+{
+	?>
+	<?php echo Kohana::lang('ui_main.no_reports'); ?>
+	<?php
+}
+foreach ($incidents as $incident)
+{
+	$incident_id = $incident->id;
+	$incident_title = text::limit_chars(strip_tags($incident->incident_title), 40, '...', True);
+	$incident_date = $incident->incident_date;
+	$incident_date = date('M j Y', strtotime($incident->incident_date));
+	$incident_location = $incident->location->location_name;
+?>
+<article class="highlight cf">
+	<img src="/media/img/report-thumb-default.jpg" class="highlight-thumb" />
+	<div class="highlight-summary">
+		<h1><a href="<?php echo url::site() . 'reports/view/' . $incident_id; ?>"> <?php echo html::specialchars($incident_title) ?></a></h1>
+		<p class="metadata">By John Doe, <span class="date"><?php echo $incident_date; ?></span></p>
+	</div>
+</article>
+<?php
+}
+?>
+
+<a class="more" href="<?php echo url::site() . 'reports/' ?>"><?php echo Kohana::lang('ui_main.view_more'); ?></a>
+
+<?php blocks::close();?>
