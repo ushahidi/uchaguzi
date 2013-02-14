8fd27e29c31a719120f5726e44aee7583af9ca73
Linda Kamau <kamaulynder@gmail.com>
2013-02-14 09:55:20 +0300

    Minor fixes to the info tab

--- application/controllers/info.php
+++ application/controllers/info.php
@@ -22,62 +22,40 @@ class Info_Controller extends Main_Controller {
 
 	public function index($page_id=1)
 	{
-	
-		$menu = "";
-		
+		$this->template->header->this_page = "page_".$page_id;
+		$this->template->content = new View('info');
+
+		if ( ! $page_id)
+		{
+			url::redirect('main');
+		}
 		$page = ORM::factory('page',$page_id)->find($page_id);
 		if ($page->loaded)
 		{
 			$page_title = $page->page_title;
 			$page_description = $page->page_description;
-			$page_id = $page->id;
 		
-			
-		}
-
-		if ($page_id=1)
-		
-		{
-
-			
-			$this->template->header->this_page = "page_".$page_id;
-			$this->template->content = new View('info');
-			
 			Event::run('ushahidi_filter.page_title', $page_title);
 			Event::run('ushahidi_filter.page_description', $page_description);
-
+	
 			$this->template->content->page_title = $page_title;
 			$this->template->content->page_description = $page_description;
 			$this->template->content->page_id = $page->id;
+		
 		}
-		
-		else if ($page_id=2)
-		
-		{
-			
-			$this->template->header->this_page = "page_".$page_id;
-			$this->template->content = new View('info');
-			
-			Event::run('ushahidi_filter.page_title', $page_title);
-			Event::run('ushahidi_filter.page_description', $page_description);
-
-			$this->template->content->page_title = $page_title;
-			$this->template->content->page_description = $page_description;
-			$this->template->content->page_id = $page->id;
-		}
-		
 		else
 		{
-			url::redirect('main');
+			url::redirect('main');	
 		}
+		
 
 		$this->template->header->page_title .= $page_title.Kohana::config('settings.title_delimiter');
 
 		$this->template->header->header_block = $this->themes->header_block();
 		$this->template->footer->footer_block = $this->themes->footer_block();
 
-
-		$this->template->content->this_page = url::site().'info/index/'.$page_id;	
+		//set the value of $this_page
+		$this->template->content->this_page = "page_".$page_id;
 	}
 
 }
--- application/helpers/uchaguzi.php
+++ application/helpers/uchaguzi.php
@@ -16,22 +16,23 @@ class uchaguzi_Core {
 	 * @return string $menu
      */
 	
-	public function info_tabs($this_page)
+	public function info_tabs($this_page = FALSE)
 	{
 		$pages = ORM::factory('page')->where('page_active','1')->find_all();
 		$menu = "";
+	
+		//All custom pages
 		
 		foreach ($pages as $page)
 		{
-			$page_id = $page->id;
+			//$page_id = $page->id;
 			$page_title = $page->page_title;
 			$page_tab = $page->page_tab;
 		
 			//Page details
-			$menu .= "<li><a href=\"".url::site()."info/index/".$page_id."\" ";
-			$menu .= ($this_page == url::site()."info/index/".$page_id) ? " class=\"active\"" : "";
-		 	$menu .= ">".$page_tab."</a></li>";
-
+			$menu .= "<li><a href=\"".url::site()."info/index/".$page->id."\" ";
+			$menu .= ($this_page == 'page_'.$page->id) ? " class=\"active\"" : "";
+			$menu .= ">".$page_tab."</a></li>";
 		}
 
 		echo $menu;
