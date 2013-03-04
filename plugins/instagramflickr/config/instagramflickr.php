b4b8f21c6416e15a666e54da2d3dad2460222cda
Henry Addo <addhen@linuxmail.org>
2013-03-04 15:31:09 +0000

    Add instagramflickr plugin

---
+++ plugins/instagramflickr/config/instagramflickr.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Instagram and Flickr api configuration
+ *
+ * PHP version 5
+ * LICENSE: This source file is subject to LGPL license 
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/copyleft/lesser.html
+ * @author	   Ushahidi Team <team@ushahidi.com> 
+ * @package	   Ushahidi - http://source.ushahididev.com
+ * @copyright  Ushahidi - http://www.ushahidi.com
+ * @license	   http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL) 
+ */
+
+$config['flick_api_key'] = "f24de49042aaf8cdd399e92df61d496";
+
+$config['instagram_client_id'] = "95f2f91ba42b4159b76042b13bbc3559";
+
+$config['instagram_tag'] = "uchaguzi";
---
+++ plugins/instagramflickr/libraries/instagramflickr_install.php
@@ -0,0 +1,95 @@
+<?php 
+
+class Instagramflickr_Install {
+
+	/**
+	 * Initialize the shared database library
+	 */
+	public function __construct() 
+	{
+		$this->db = Database::instance();
+	}
+
+	/**
+	 * Install the database
+	 * 
+	 */
+	public function run_install() 
+	{
+
+		// Create the database tables.
+		// Also include table_prefix in name
+		$this->db->query('CREATE TABLE IF NOT EXISTS `'.Kohana::config('database.default.table_prefix'). 'instagramflickr` (
+		  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+		  `parent_id` bigint(20) DEFAULT \'0\',
+		  `incident_id` bigint(20) unsigned DEFAULT \'0\',
+		  `user_id` int(11) unsigned DEFAULT \'0\',
+		  `reporter_id` bigint(20) unsigned DEFAULT NULL,
+		  `service_photoid` varchar(100) DEFAULT NULL,
+		  `photo_from` varchar(100) DEFAULT NULL,
+		  `photo_to` varchar(100) DEFAULT NULL,
+		  `photo_title` text,
+		  `photo_description` text,
+		  `photo_type` tinyint(4) DEFAULT \'1\' COMMENT \'1 - INBOX, 2 - OUTBOX (From Admin), 3 - DELETED\',
+		  `photo_date` datetime DEFAULT NULL,
+		  `photo_level` tinyint(4) DEFAULT \'0\' COMMENT \'0 - UNREAD, 1 - READ, 99 - SPAM\',
+		  `latitude` double DEFAULT NULL,
+		  `longitude` double DEFAULT NULL,
+		  PRIMARY KEY (`id`),
+		  KEY `user_id` (`user_id`),
+		  KEY `incident_id` (`incident_id`),
+		  KEY `reporter_id` (`reporter_id`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT=\'Stores instagram and flickr photo details\' AUTO_INCREMENT=1 ;');
+
+		// Create the settings tables.
+		// Also include table_prefix in name
+		$this->db->query('CREATE TABLE IF NOT EXISTS `'.Kohana::config('database.default.table_prefix').'instagramflickr_settings` (
+			`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  			`flickr_tag` varchar(255) DEFAULT NULL,
+  			`flickr_id` varchar(15) DEFAULT NULL,
+  			`num_of_photos` tinyint(4) NOT NULL DEFAULT \'0\',
+  			`image_width` int(11) NOT NULL DEFAULT \'500\',
+  			`image_height` int(11) NOT NULL DEFAULT \'375\',
+  			`block_position` varchar(15) DEFAULT NULL,
+  			`enable_cache` int(5) NOT NULL,
+  			`block_no_photos` int(5) NOT NULL,
+  			PRIMARY KEY (`id`)
+		) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1');
+
+		// Check for flickr service
+		$flickr = $this->db->query('SELECT service_name FROM `'.Kohana::config('database.default.table_prefix').'service` WHERE service_name=\'Flickr\'');
+		
+		if($flickr->count() == 0)
+		{ 	
+			// Insert flickr service
+			$this->db->query('INSERT INTO `'.Kohana::config('database.default.table_prefix')
+				.'service` ( 
+					`service_name`, `service_description`, `service_url`, 
+					`service_api`) VALUES 
+			(\'Flickr\', \'Flick Photos\', \'http://flickr.com\', NULL)');
+		}
+
+		$instagram = $this->db->query('SELECT service_name FROM `'.Kohana::config('database.default.table_prefix').'service` WHERE service_name=\'Instagram\'');
+
+		if($instagram->count() == 0)
+		{
+			// Insert instagram service
+			$this->db->query('INSERT INTO `'.Kohana::config('database.default.table_prefix')
+				.'service` ( 
+				`service_name`, `service_description`, `service_url`, 
+				`service_api`) VALUES 
+			(\'Instagram\', \'Instagram Photos\', \'http://instagram.com/\', NULL)');
+		}
+
+	}
+
+	/**
+	 * Deletes the database tables for the actionable module
+	 */
+	public function uninstall()
+	{
+		$this->db->query('DROP TABLE `'.Kohana::config('database.default.table_prefix').'instagramflickr');
+
+		$this->db->query('DROP TABLE `'.Kohana::config('database.default.table_prefix').'instagramflickr_settings');
+	}
+}
